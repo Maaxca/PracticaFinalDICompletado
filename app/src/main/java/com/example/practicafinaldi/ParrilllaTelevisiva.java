0ebbc149b54cbb14f749e5515a8a63e18caa99ce
Maaxca <mario@gmail.com>
2021-12-13 17:39:28 +0100

    El final llegó y la navidad se acercó ya que una ps5 me llevó al espantoso ostion de la defensa del cojon

--- app/src/main/java/com/example/practicafinaldi/ParrilllaTelevisiva.java
+++ app/src/main/java/com/example/practicafinaldi/ParrilllaTelevisiva.java
@@ -1,16 +1,23 @@
 package com.example.practicafinaldi;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import org.w3c.dom.Attr;
@@ -37,12 +44,14 @@ public class ParrilllaTelevisiva extends AppCompatActivity {
     ListView lista;
     ArrayList<String>Canales=new ArrayList<>();
     ArrayList<String>nombre=new ArrayList<>();
+    ArrayList<Integer>Fotos=new ArrayList();
     public int cont=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parrillla_televisiva);
         lista=findViewById(R.id.lista2);
+        rellenarFotos();
         DescargarXML descargarXML = new DescargarXML();
         descargarXML.execute("https://raw.githubusercontent.com/dracohe/CARLOS/master/guide_IPTV.xml");
         lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -54,11 +63,267 @@ public class ParrilllaTelevisiva extends AppCompatActivity {
             }
         });
     }
+    void rellenarFotos(){
+        Fotos.add(R.drawable.uno);
+        Fotos.add(R.drawable.uno);
+        Fotos.add(R.drawable.uno);
+        Fotos.add(R.drawable.dos);
+        Fotos.add(R.drawable.dos);
+        Fotos.add(R.drawable.tres);
+        Fotos.add(R.drawable.tres);
+        Fotos.add(R.drawable.tres);
+        Fotos.add(R.drawable.cuatro);
+        Fotos.add(R.drawable.cuatro);
+        Fotos.add(R.drawable.cuatro);
+        Fotos.add(R.drawable.cinco);
+        Fotos.add(R.drawable.cinco);
+        Fotos.add(R.drawable.cinco);
+        Fotos.add(R.drawable.sexta);
+        Fotos.add(R.drawable.sexta);
+        Fotos.add(R.drawable.sexta);
+        Fotos.add(R.drawable.cero);
+        Fotos.add(R.drawable.cero);
+        Fotos.add(R.drawable.cero);
+        Fotos.add(R.drawable.estrenos);
+        Fotos.add(R.drawable.estrenos);
+        Fotos.add(R.drawable.estrenos2);
+        Fotos.add(R.drawable.fest);
+        Fotos.add(R.drawable.clasicos);
+        Fotos.add(R.drawable.accion);
+        Fotos.add(R.drawable.accion);
+        Fotos.add(R.drawable.accion);
+        Fotos.add(R.drawable.comedia);
+        Fotos.add(R.drawable.comedia);
+        Fotos.add(R.drawable.comedia);
+        Fotos.add(R.drawable.drama);
+        Fotos.add(R.drawable.drama);
+        Fotos.add(R.drawable.drama);
+        Fotos.add(R.drawable.cinee);
+        Fotos.add(R.drawable.cinee);
+        Fotos.add(R.drawable.series);
+        Fotos.add(R.drawable.series2);
+        Fotos.add(R.drawable.tnt);
+        Fotos.add(R.drawable.tnt);
+        Fotos.add(R.drawable.tnt);
+        Fotos.add(R.drawable.tcm);
+        Fotos.add(R.drawable.tcm);
+        Fotos.add(R.drawable.syfy);
+        Fotos.add(R.drawable.syfy);
+        Fotos.add(R.drawable.syfy);
+        Fotos.add(R.drawable.sundance);
+        Fotos.add(R.drawable.sundance);
+        Fotos.add(R.drawable.hollywood);
+        Fotos.add(R.drawable.hollywood);
+        Fotos.add(R.drawable.fox);
+        Fotos.add(R.drawable.fox);
+        Fotos.add(R.drawable.fox);
+        Fotos.add(R.drawable.fox);
+        Fotos.add(R.drawable.foxhd);
+        Fotos.add(R.drawable.foxhd);
+        Fotos.add(R.drawable.foxhd);
+        Fotos.add(R.drawable.dark);
+        Fotos.add(R.drawable.dark);
+        Fotos.add(R.drawable.cosmo);
+        Fotos.add(R.drawable.cosmo);
+        Fotos.add(R.drawable.cosmo);
+        Fotos.add(R.drawable.comedycentral);
+        Fotos.add(R.drawable.comedycentral);
+        Fotos.add(R.drawable.calle);
+        Fotos.add(R.drawable.calle);
+        Fotos.add(R.drawable.calle);
+        Fotos.add(R.drawable.axn);
+        Fotos.add(R.drawable.axn);
+        Fotos.add(R.drawable.axn);
+        Fotos.add(R.drawable.axnwhite);
+        Fotos.add(R.drawable.axnwhite);
+        Fotos.add(R.drawable.axnwhite);
+        Fotos.add(R.drawable.amc);
+        Fotos.add(R.drawable.amc);
+        Fotos.add(R.drawable.amc);
+        Fotos.add(R.drawable.atres);
+        Fotos.add(R.drawable.atres);
+        Fotos.add(R.drawable.crimen);
+        Fotos.add(R.drawable.crimen);
+        Fotos.add(R.drawable.dream);
+        Fotos.add(R.drawable.fdf);
+        Fotos.add(R.drawable.fdf);
+        Fotos.add(R.drawable.fdf);
+        Fotos.add(R.drawable.energy);
+        Fotos.add(R.drawable.divinity);
+        Fotos.add(R.drawable.paramount);
+        Fotos.add(R.drawable.paramount);
+        Fotos.add(R.drawable.mtv);
+        Fotos.add(R.drawable.mtv);
+        Fotos.add(R.drawable.nova);
+        Fotos.add(R.drawable.nova);
+        Fotos.add(R.drawable.neox);
+        Fotos.add(R.drawable.neox);
+        Fotos.add(R.drawable.disneych);
+        Fotos.add(R.drawable.disneych);
+        Fotos.add(R.drawable.disneyjunior);
+        Fotos.add(R.drawable.disneyjunior);
+        Fotos.add(R.drawable.baby);
+        Fotos.add(R.drawable.nick);
+        Fotos.add(R.drawable.nick);
+        Fotos.add(R.drawable.nickelodeon);
+        Fotos.add(R.drawable.nickelodeon);
+        Fotos.add(R.drawable.clan);
+        Fotos.add(R.drawable.clan);
+        Fotos.add(R.drawable.panda);
+        Fotos.add(R.drawable.panda);
+        Fotos.add(R.drawable.boing);
+        Fotos.add(R.drawable.xtrm);
+        Fotos.add(R.drawable.somos);
+        Fotos.add(R.drawable.solmusica);
+        Fotos.add(R.drawable.beti);
+        Fotos.add(R.drawable.real);
+        Fotos.add(R.drawable.sevilla);
+        Fotos.add(R.drawable.barsa);
+        Fotos.add(R.drawable.barsa);
+        Fotos.add(R.drawable.vamos);
+        Fotos.add(R.drawable.vamos);
+        Fotos.add(R.drawable.vamos);
+        Fotos.add(R.drawable.gol);
+        Fotos.add(R.drawable.gol);
+        Fotos.add(R.drawable.gol);
+        Fotos.add(R.drawable.mvliga);
+        Fotos.add(R.drawable.mvliga);
+        Fotos.add(R.drawable.mvliga);
+        Fotos.add(R.drawable.mvliga);
+        Fotos.add(R.drawable.ligauno);
+        Fotos.add(R.drawable.ligados);
+        Fotos.add(R.drawable.ligatres);
+        Fotos.add(R.drawable.liga4);
+        Fotos.add(R.drawable.ligacinco);
+        Fotos.add(R.drawable.ligaseis);
+        Fotos.add(R.drawable.ligasiete);
+        Fotos.add(R.drawable.ligabar);
+        Fotos.add(R.drawable.orange);
+        Fotos.add(R.drawable.campeones);
+        Fotos.add(R.drawable.campeones);
+        Fotos.add(R.drawable.campeones);
+        Fotos.add(R.drawable.campeones);
+        Fotos.add(R.drawable.campeones);
+        Fotos.add(R.drawable.campeonesuno);
+        Fotos.add(R.drawable.campeonesuno);
+        Fotos.add(R.drawable.campeonesuno);
+        Fotos.add(R.drawable.campeonesuno);
+        Fotos.add(R.drawable.campeonesdos);
+        Fotos.add(R.drawable.campeonesdos);
+        Fotos.add(R.drawable.campeonesdos);
+        Fotos.add(R.drawable.campeonestres);
+        Fotos.add(R.drawable.campeonestres);
+        Fotos.add(R.drawable.campeonestres);
+        Fotos.add(R.drawable.campeonescuatro);
+        Fotos.add(R.drawable.campeonescuatro);
+        Fotos.add(R.drawable.campeonescuatro);
+        Fotos.add(R.drawable.campeones5);
+        Fotos.add(R.drawable.campeones6);
+        Fotos.add(R.drawable.canpeonessiete);
+        Fotos.add(R.drawable.daznf);
+        Fotos.add(R.drawable.garege);
+        Fotos.add(R.drawable.garege);
+        Fotos.add(R.drawable.tdp);
+        Fotos.add(R.drawable.tdp);
+        Fotos.add(R.drawable.golf);
+        Fotos.add(R.drawable.golf);
+        Fotos.add(R.drawable.eurosport);
+        Fotos.add(R.drawable.eurosportdos);
+        Fotos.add(R.drawable.deportes);
+        Fotos.add(R.drawable.deportes);
+        Fotos.add(R.drawable.deportes);
+        Fotos.add(R.drawable.mtvh);
+        Fotos.add(R.drawable.mtv);
+        Fotos.add(R.drawable.mtv);
+        Fotos.add(R.drawable.toros);
+        Fotos.add(R.drawable.toros);
+        Fotos.add(R.drawable.toros);
+        Fotos.add(R.drawable.cazavision);
+        Fotos.add(R.drawable.iberalia);
+        Fotos.add(R.drawable.caza);
+        Fotos.add(R.drawable.caza);
+        Fotos.add(R.drawable.caza);
+        Fotos.add(R.drawable.viajar);
+        Fotos.add(R.drawable.viajar);
+        Fotos.add(R.drawable.ten);
+        Fotos.add(R.drawable.ten);
+        Fotos.add(R.drawable.odisea);
+        Fotos.add(R.drawable.odisea);
+        Fotos.add(R.drawable.nat);
+        Fotos.add(R.drawable.nat);
+        Fotos.add(R.drawable.wild);
+        Fotos.add(R.drawable.wild);
+        Fotos.add(R.drawable.wild);
+        Fotos.add(R.drawable.wild);
+        Fotos.add(R.drawable.inter);
+        Fotos.add(R.drawable.dkiss);
+        Fotos.add(R.drawable.mega);
+        Fotos.add(R.drawable.mega);
+        Fotos.add(R.drawable.historia);
+        Fotos.add(R.drawable.historia);
+        Fotos.add(R.drawable.dmax);
+        Fotos.add(R.drawable.dmax);
+        Fotos.add(R.drawable.discovery);
+        Fotos.add(R.drawable.discovery);
+        Fotos.add(R.drawable.decasa);
+        Fotos.add(R.drawable.decasa);
+        Fotos.add(R.drawable.cocina);
+        Fotos.add(R.drawable.cocina);
+        Fotos.add(R.drawable.blaze);
+        Fotos.add(R.drawable.blaze);
+        Fotos.add(R.drawable.bemad);
+        Fotos.add(R.drawable.bemad);
+        Fotos.add(R.drawable.trece);
+        Fotos.add(R.drawable.horas);
+        Fotos.add(R.drawable.canalsur);
+        Fotos.add(R.drawable.canalsur);
+        Fotos.add(R.drawable.andalucia);
+        Fotos.add(R.drawable.tvg);
+        Fotos.add(R.drawable.tvg);
+        Fotos.add(R.drawable.tvg);
+        Fotos.add(R.drawable.telemadrid);
+        Fotos.add(R.drawable.telemadrid);
+        Fotos.add(R.drawable.laotra);
+        Fotos.add(R.drawable.casman);
+        Fotos.add(R.drawable.casman);
+        Fotos.add(R.drawable.etb);
+        Fotos.add(R.drawable.etb);
+        Fotos.add(R.drawable.etb);
+        Fotos.add(R.drawable.extre);
+        Fotos.add(R.drawable.extre);
+        Fotos.add(R.drawable.cat);
+        Fotos.add(R.drawable.asturias);
+        Fotos.add(R.drawable.aragon);
+        Fotos.add(R.drawable.aragon);
+        Fotos.add(R.drawable.siete);
+        Fotos.add(R.drawable.punt);
+        Fotos.add(R.drawable.bom);
+        Fotos.add(R.drawable.beteve);
+        Fotos.add(R.drawable.baleares);
+        Fotos.add(R.drawable.baleares);
+        Fotos.add(R.drawable.canarias);
+        Fotos.add(R.drawable.murcia);
+        Fotos.add(R.drawable.esport);
+        Fotos.add(R.drawable.ocho);
+        Fotos.add(R.drawable.cadiz);
+        Fotos.add(R.drawable.levante);
+        Fotos.add(R.drawable.murcia);
+        Fotos.add(R.drawable.dazn);
+        Fotos.add(R.drawable.dazn2);
+        Fotos.add(R.drawable.dazn3);
+        Fotos.add(R.drawable.dazn4);
+        Fotos.add(R.drawable.redbull);
+        Fotos.add(R.drawable.deportes);
+        Fotos.add(R.drawable.dazn);
+        Fotos.add(R.drawable.resistencia);
+        Fotos.add(R.drawable.biogra);
+        Fotos.add(R.drawable.nautical);
 
+    }
     private class DescargarXML extends AsyncTask<String, Void, Void> {
         String todo="" ;
         ArrayAdapter<String> adapter;
-        List<String> list;
+        ArrayList<String> list;
 
         @Override
         protected void onPreExecute() {
@@ -73,7 +338,7 @@ public class ParrilllaTelevisiva extends AppCompatActivity {
         @Override
         protected void onPostExecute(Void unused) {
             super.onPostExecute(unused);
-            adapter = new ArrayAdapter<String>(getApplicationContext(), R.layout.support_simple_spinner_dropdown_item, list);
+            adapter = new ParrilllaTelevisiva.AdaptadorParaSeries1(getApplicationContext(),R.layout.plantilla_series,list);
             lista.setAdapter(adapter);
             progressDialog.dismiss();
         }
@@ -108,7 +373,7 @@ public class ParrilllaTelevisiva extends AppCompatActivity {
                         if (nodo instanceof Element ) {
                             if(nodo.getNodeName().compareTo("channel")==0) {
                                 NodeList nietos = nodo.getChildNodes();
-                                for (int j = 0; j < nietos.getLength(); j++) {
+                                for (int j = 0; j < nietos.getLength()-2; j++) {
                                     todo += nietos.item(j).getTextContent();
                                     if(nietos.item(j).getNodeName().compareTo("display-name")==0){
                                         nombre.add(nietos.item(j).getTextContent());
@@ -232,5 +497,39 @@ public class ParrilllaTelevisiva extends AppCompatActivity {
             }
             return null;
         }
+    }
+    private class AdaptadorParaSeries1 extends ArrayAdapter<String> {
+
+        public AdaptadorParaSeries1(@NonNull Context context, int resource, @NonNull ArrayList<String> objects) {
+            super(context, resource, objects);
+
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            return rellenarFila(position, convertView, parent);
+        }
+
+        @Override
+        public View getDropDownView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            return rellenarFila(position, convertView, parent);
+        }
+
+        public View rellenarFila(int posicion, View view, ViewGroup padre) {
+
+            LayoutInflater inflater = getLayoutInflater();
+            View mifila = inflater.inflate(R.layout.plantilla_series, padre, false);
+
+            TextView title = mifila.findViewById(R.id.textViewTituloS);
+            title.setText(nombre.get(posicion));
+
+            ImageView logo = mifila.findViewById(R.id.imageView);
+            logo.setImageResource(Fotos.get(posicion));
+
+            return mifila;
+        }
+
+
     }
 }
